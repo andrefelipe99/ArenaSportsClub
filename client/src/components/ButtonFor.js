ab6ced49b7c6e322f8ce59815300dc5519e5faeb
André Felipe <dede.f99.80@gmail.com>
2023-01-21 17:01:51 -0300

    map dos jogadoress

--- client/src/components/ButtonFor.js
+++ client/src/components/ButtonFor.js
@@ -5,30 +5,31 @@ function ButtonForMatch({ actived, listMatch }) {
   return (
     actived && (
       <Container id="container-buttonFor">
-        <div>
-          {typeof listMatch.partida === "undefined" ? (
-            <p>Loading...</p>
-          ) : (
-            <>
-              <div id="header-for">
-                <span>{listMatch.partida[0].formacaoCasa}</span>
-                <span> FORMAÇÃO </span>
-                <span>{listMatch.partida[0].formacaoFora}</span>
-              </div>
+        <div id="main-header-for">
+          <div id="header-for">
+            <span>{listMatch.partida[0].formacaoCasa}</span>
+            <span> FORMAÇÃO </span>
+            <span>{listMatch.partida[0].formacaoFora}</span>
+          </div>
 
-              <div className="content-fieldWrap">
-                <div className="content-field">
-                  <div className="field-line">
-                    <div className="field-player">
-                      {" "}
-                      {listMatch.partida[0].escalacaoCasa[0].numero}{" "}
-                      <span>{listMatch.partida[0].escalacaoCasa[0].nome}</span>
-                    </div>
-                  </div>
-                </div>
+          <div className="content-fieldWrap">
+            <div className="content-field">
+              <div className="field-line">
+                {typeof listMatch.partida === "undefined" ? (
+                  <p>Loading...</p>
+                ) : (
+                  listMatch.partida[0].escalacaoCasa.map((jogador, i) => (
+                    <>
+                      <div className="field-player" key={i}>
+                        {" "}
+                        {jogador.numero} <span>{jogador.nome}</span>
+                      </div>
+                    </>
+                  ))
+                )}
               </div>
-            </>
-          )}
+            </div>
+          </div>
         </div>
       </Container>
     )

dc92e1457b057a4a3dd01818eca3ab8f518ca4fd
Ricardox20 <sparssbh@gmail.com>
2023-01-21 17:14:39 -0300

    Ajustes na barra de favoritos

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -4,21 +4,31 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
 import { AiOutlineStar, AiFillStar } from "react-icons/ai";
+import { ImTrophy } from "react-icons/im";
+import { GiStarsStack } from "react-icons/gi";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "../styles/components/SideBar.css";
 
 export function SideBar() {
   const [listaCampeonatos, setListaCampeonatos] = useState([{}]);
-  const [favorites, setFavorites] = useState([{}]);
+  const [favorites, setFavorites] = useState([
+    { id: 0, favorito: false, nome: "", paisUrl: "" },
+  ]);
 
   const addFavorite = (campeonato) => {
     if (campeonato !== "undefined") {
       let { id, favorito, nome, paisUrl } = campeonato;
-      setFavorites((favorite) => [
-        ...favorite,
-        { id, favorito, nome, paisUrl },
-      ]);
+      console.log(favorites[0]);
+      if (favorites.length === 1 && favorites[0].id !== 0) {
+        setFavorites((favorite) => [
+          ...favorite,
+          { id, favorito, nome, paisUrl },
+        ]);
+      } else {
+        setFavorites((favorite) => [{ id, favorito, nome, paisUrl }]);
+      }
+      console.log(Object.values(favorites));
     }
   };
 
@@ -42,8 +52,65 @@ export function SideBar() {
     <Container id="container-side-bar">
       <div>
         <div className="titleSideBar">
-          <AiFillStar />
-          <span id="title-text-side-bar">Minhas ligas</span>
+          <GiStarsStack />
+          <span id="title-text-side-bar">Minhas Ligas</span>
+        </div>
+        {favorites[0]?.id === 0 || favorites.length === 0 ? (
+          <span className="titleSideBar" id="title-text-side-bar">
+            Nenhum Campeonato Adicionado
+          </span>
+        ) : (
+          favorites.map((favorito, i) => (
+            <div key={i}>
+              <>
+                <Link to="/" id="side-bar-link">
+                  <ListGroup>
+                    <ListGroup.Item id="list-group-sidebar">
+                      <Container>
+                        <Row className="justify-content-md-center">
+                          <Col md={2}>
+                            <img
+                              className="pais-margin"
+                              src={favorito.paisUrl}
+                              alt={`${favorito.paisUrl}`}
+                              width="25"
+                            />
+                          </Col>
+                          <Col
+                            md={8}
+                            id="name-camp-sidebar"
+                            title={favorito.nome}
+                          >
+                            <span>{favorito.nome}</span>
+                          </Col>
+                          <Col md={2}>
+                            <Button
+                              id="button-favorite"
+                              onClick={() =>
+                                isFavorite(favorito)
+                                  ? removeFavorite(favorito)
+                                  : addFavorite(favorito)
+                              }
+                            >
+                              {isFavorite(favorito) ? (
+                                <AiFillStar />
+                              ) : (
+                                <AiOutlineStar />
+                              )}
+                            </Button>
+                          </Col>
+                        </Row>
+                      </Container>
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Link>
+              </>
+            </div>
+          ))
+        )}
+        <div className="titleSideBar">
+          <ImTrophy />
+          <span id="title-text-side-bar">Principais Campeonatos</span>
         </div>
         {typeof listaCampeonatos.campeonatos === "undefined" ? (
           <p>Loading...</p>

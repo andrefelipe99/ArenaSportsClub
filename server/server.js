18c6ff0fd4b20b486306f908924b8260240d242f
Thiago Luiz <tluiz199@gmail.com>
2023-03-23 14:52:13 -0300

    testes para corrigir erro vercel back

--- server/server.js
+++ server/server.js
@@ -2,10 +2,8 @@ import express from "express";
 import cors from "cors";
 import football from "./src/api/football.route.js";
 import matchsCrawler from "./src/crawler/matchs.js";
-import matchsController from "./src/api/matchs.controller.js";
 import championshipsCrawler from "./src/crawler/championships.js";
 import newsCrawler from "./src/crawler/news.js";
-import newsController from "./src/api/news.controller.js";
 
 const app = express();
 
@@ -14,12 +12,12 @@ app.use(express.json());
 app.use("/api/v1/football", football);
 //app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
 
-const championships = await championshipsCrawler.getChampionships();
-const news = await newsCrawler.getNews();
-const matchs = await matchsCrawler.getMatchs();
-app.get("/pp", (req, res) => {
-  res.json({ news });
-});
+// const championships = await championshipsCrawler.getChampionships();
+// const news = await newsCrawler.getNews();
+// const matchs = await matchsCrawler.getMatchs();
+// app.get("/pp", (req, res) => {
+//   res.json({ news });
+// });
 
 // setInterval(async () => {
 //   const news = await newsController.apiPostNews();
--- server/src/api/football.route.js
+++ server/src/api/football.route.js
@@ -1,4 +1,4 @@
-import express, { response } from "express";
+import express from "express";
 import matchsCtrl from "./matchs.controller.js";
 import teamsCtrl from "./teams.controller.js";
 import championshipsCtrl from "./championships.controller.js";

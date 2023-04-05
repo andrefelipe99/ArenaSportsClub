f97a3ccb9860b05541d3a0aa4e924146c94bac5a
Thiago Luiz <tluiz199@gmail.com>
2023-04-05 10:07:29 -0300

    correcoes status match e footer

--- client/src/components/Default/Footer.js
+++ client/src/components/Default/Footer.js
@@ -9,12 +9,12 @@ export function Footer() {
     <MDBFooter id="footer" className="text-center text-lg-start">
       <Container>
         <Row>
-          <Col md={3} lg="4" xl="4" className="footer-div mx-auto mb-4">
+          <Col md={4} lg={4} xl={4} className="footer-div mx-auto mb-4">
             <h6 className="text-uppercase fw-bold mb-3">© Arena Sport Club</h6>
             <span>Projeto criado com finalidade acadêmica.</span>
           </Col>
 
-          <Col md="4" lg="3" xl="4" className="footer-div mx-auto mb-md-0 mb-4">
+          <Col md={4} lg={4} xl={4} className="footer-div mx-auto mb-md-0 mb-4">
             <h6 className="text-uppercase fw-bold mb-3">Contato</h6>
             <a
               href="https://github.com/andrefelipe99/ArenaSportsClub"

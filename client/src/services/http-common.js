89c46eb36d90aea15626698b5e38d83d018742b0
Thiago Luiz <tluiz199@gmail.com>
2023-04-14 12:21:49 -0300

    fix header front para aceitar todas as requisições

--- client/src/services/http-common.js
+++ client/src/services/http-common.js
@@ -5,5 +5,6 @@ export default axios.create({
   baseURL: "https://arena-sports-club-api.vercel.app/api/v1/football",
   headers: {
     "Content-type": "application/json",
+    "Access-Control-Allow-Origin": "*",
   },
 });

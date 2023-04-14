aa98c7e5f488199d98ae8981ee449443a1ccc3a6
Thiago Luiz <tluiz199@gmail.com>
2023-04-14 12:31:34 -0300

    fix middleware no back para aceitar todas requisições

--- server/server.js
+++ server/server.js
@@ -14,6 +14,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.use("/api/v1/football", football);
 //app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
 

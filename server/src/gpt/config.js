07996d07af75055c2e2cfe2901d7bdfff9ae46ec
Thiago Luiz <tluiz199@gmail.com>
2023-08-16 20:33:08 -0300

    adicionado linguagem dinâmica na resposta do prompt

--- server/src/gpt/config.js
+++ server/src/gpt/config.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export async function GPTconfiguration(localization) {
+export async function GPTconfiguration(localization, language) {
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   });
@@ -18,12 +18,14 @@ export async function GPTconfiguration(localization) {
           content:
             "Você é um especialista de futebol e de localidades geográficas, primeiramente irei te informar coordenadas \
             de GPS e sua única atividade será encontrar equipes de futebol próximas a essas coordenadas informadas. \
-            Você não precisa explicar que você possui conhecimento apenas até 2021, basta apenas informar em forma \
-            de lista as equipes e uma breve descrição sobre elas. As coordenadas serão enviadas separadas por três ---",
+            Você deverá me retornar em forma de lista enumerada, e apenas essa lista com as equipes e uma breve descrição sobre elas. \
+            Não envie nenhuma mensagem sobre você ter acesso a informações até 2021 e também não solicite novas coordenadas. \
+            Também irei te informar a língua que você precisa me responder. As coordenadas serão enviadas separadas por três --- \
+            e a linguagem desejada separada por três ***",
         },
         {
           role: "user",
-          content: `Estas são minhas coordenadas --- ${localization[0]}, ${localization[1]} ---`,
+          content: `Estas são minhas coordenadas --- ${localization[0]}, ${localization[1]} --- e essa é a linguagem da resposta *** ${language} ***`,
         },
       ],
     });
@@ -42,8 +44,8 @@ export async function GPTconfiguration(localization) {
 export async function getLocalizationResponse(req, res, next) {
   try {
     let body = req.body;
-    let { localization } = body;
-    const response = await GPTconfiguration(localization);
+    let { localization, language } = body;
+    const response = await GPTconfiguration(localization, language);
 
     res.status(200).json({
       data: response,
